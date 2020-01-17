0977d106a44885256b969e9b279f2fdb83b50a38
Xavier Simó <xavier.simo@xing.com>
2020-01-17 18:49:59 +0100

    Substitute Ansible for a shell script
    Now that the Brewfile exists listing all brew dependencies and since I
    haven't used ansible in years for anything else than this it will be one
    less dependency installed.

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,10 +1,28 @@
 #! /bin/sh
 
+# Run macOS terminal tweaks
+sh ./macos/tweaks.sh
+
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install Ansible
-brew install ansible
+ln -s $(pwd)/Brewfile ~/Brewfile
 
-# Run ansible dev env playbook
-ansible-playbook ansible/provide_dev_env.yml -i ansible/hosts
+brew bundle install
+
+# make zsh my default shell
+chsh -s /bin/zsh Xavier
+
+mkdir -p ~/.vim/colors ~/.vim/ftplugin
+
+# link actual dotfiles
+ln -s -f $(pwd)/ruby/rspec ~/.rspec
+ln -s -f $(pwd)/vim/vimrc ~/.vimrc
+ln -s -f $(pwd)/vim/angr.vim ~/.vim/colors/angr.vim
+ln -s -f $(pwd)/vim/ftplugin ~/.vim/ftplugin
+ln -s -f $(pwd)/zsh/zshrc ~/.zshrc
+ln -s -f $(pwd)/tmux/tmux.conf ~/.tmux.conf
+ln -s -f $(pwd)/global_gitignore ~/.gitignore
+
+# install nvim plugins
+nvim +PluginInstall +qall
