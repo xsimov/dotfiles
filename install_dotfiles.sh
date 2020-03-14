657de42cf0b204ccb9be33807e81cc463012f455
Xavier Simó <xavier.simo@xing.com>
2020-03-14 13:17:23 +0100

    Update after trying the new structure for the first time :smile:

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -3,6 +3,7 @@
 # Run macOS terminal tweaks
 sh ./macos/tweaks.sh
 
+# add an if to avoid installing it more than once
 # Install homebrew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -10,19 +11,26 @@ ln -s $(pwd)/Brewfile ~/Brewfile
 
 brew bundle install
 
-# make zsh my default shell
-chsh -s /bin/zsh Xavier
-
-mkdir -p ~/.vim/colors ~/.vim/ftplugin
+mkdir -p ~/.vim/colors ~/.vim/ftplugin ~/.config/nvim/colors
 
 # link actual dotfiles
 ln -s -f $(pwd)/ruby/rspec ~/.rspec
 ln -s -f $(pwd)/vim/vimrc ~/.vimrc
+ln -s -f $(pwd)/vim/vimrc ~/.config/nvim/init.vim
 ln -s -f $(pwd)/vim/angr.vim ~/.vim/colors/angr.vim
+ln -s -f $(pwd)/vim/angr.vim ~/.config/nvim/colors/angr.vim
 ln -s -f $(pwd)/vim/ftplugin ~/.vim/ftplugin
 ln -s -f $(pwd)/zsh/zshrc ~/.zshrc
 ln -s -f $(pwd)/tmux/tmux.conf ~/.tmux.conf
 ln -s -f $(pwd)/global_gitignore ~/.gitignore
+ln -s -f $(pwd)/gitconfig ~/.gitconfig
+
+# install oh-my-zsh
+# MISSING!
+
+ln -s -f $(pwd)/zsh/avit_modified.zsh-theme ~/.oh-my-zsh/themes/avit_modified.zsh-theme
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # install nvim plugins
 nvim +PluginInstall +qall
